8e5bf30bf9c4f24b31fea42a602b7f0efbaceae6
Ammar Aziz <spidermine63@gmail.com>
2024-03-08 17:10:49 +1100

    upstream: Align trim (#2)
    * adding --min-depth flag to minion and parameterising the mask and stats calls with it
    
    * parameterising the vcf filter with the same global min-depth variable for consistency
    
    * adding github actions CI
    
    * adding github actions CI
    
    * adding github actions CI
    
    * adding github actions CI
    
    * moving unit tests over to github actions
    
    * parameterising the vcf filter with the same global min-depth variable for consistency
    
    * replacing travis with github action CI
    
    * adding DOI to README
    
    * updating artic-tools, adding more stats to artic report, running report regardless of strict, setting strict to filter VCF
    
    * adding opt to enforce full length amplicon match during align trim, activate using --strict flag to minion
    
    * tweaking report generator
    
    * version bump
    
    * docs update
    
    * replacing longshot with the latest medaka
    
    * trying to fix medaka install on osx
    
    * adding bcftools norm command to remove vars from the pass VCF if they have been masked out in the preconsensus
    
    * cleaner parsing of GQ field
    
    * upgrading to latest medaka and dropping longshot from the conda env
    
    * exposing opts for vcf_filter
    
    * bumping medaka to latest patch
    
    * unpinning pandas
    
    * Change to overlap-based method of amplicon resolution
    
    * Integrate new align_trim.py
    
    ---------
    
    Co-authored-by: Will Rowe <w.p.m.rowe@gmail.com>
    Co-authored-by: Chris Wright <chris.wright@nanoporetech.com>

--- artic/minion.py
+++ artic/minion.py
@@ -197,8 +197,22 @@ def run(parser, args):
         strict_string = '--enforce-amplicon-span'
     else:
         strict_string = ''
-    cmds.append("align_trim %s %s %s --start --remove-incorrect-pairs --report %s.alignreport.txt < %s.sorted.bam 2> %s.alignreport.er | samtools sort -T %s - -o %s.trimmed.rg.sorted.bam" % (strict_string, normalise_string, bed, args.sample, args.sample, args.sample, args.sample, args.sample))
-    cmds.append("align_trim %s %s %s --remove-incorrect-pairs --report %s.alignreport.txt < %s.sorted.bam 2> %s.alignreport.er | samtools sort -T %s - -o %s.primertrimmed.rg.sorted.bam" % (strict_string, normalise_string, bed, args.sample, args.sample, args.sample, args.sample, args.sample))
+    cmds.append(
+        "align_trim %s.sorted.bam %s "
+        "%s %s --start --report %s.alignreport.txt 2> %s.alignreport.er "
+        "| samtools sort -T %s - -o %s.trimmed.rg.sorted.bam" % (
+            args.sample, bed,
+            strict_string, normalise_string, args.sample, args.sample,
+            args.sample, args.sample)
+        )
+    cmds.append(
+        "align_trim %s.sorted.bam %s "
+        "%s %s --report %s.alignreport.txt 2> %s.alignreport.er "
+        "| samtools sort -T %s - -o %s.primertrimmed.rg.sorted.bam" % (
+            args.sample, bed,
+            strict_string, normalise_string, args.sample, args.sample,
+            args.sample, args.sample)
+        )
     cmds.append("samtools index %s.trimmed.rg.sorted.bam" % (args.sample))
     cmds.append("samtools index %s.primertrimmed.rg.sorted.bam" % (args.sample))
 
